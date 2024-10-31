5dde117b830469e70a0e49f51ef44c22f80eb71a
Dario <daritorres06@gmail.com>
2024-10-31 11:53:42 -0500

    31 Octubre del 2024 cambio de planificacion mensual a fechas segun la necesidad

--- manzana_de_cuidados/model_gestion_servicio/mz_planificacion_servicio.py
+++ manzana_de_cuidados/model_gestion_servicio/mz_planificacion_servicio.py
@@ -6,7 +6,8 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 from babel.dates import format_date
 # from datetime import datetime,time, datetime
-import datetime
+from datetime import datetime
+from datetime import date
 import time
 import json
 global NoTieneHorario
@@ -19,19 +20,16 @@ class GenerarHorarios(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Generar Horarios'
     _rec_name = 'servicio_id'
-    #_order = 'unidad_id , mes_genera'
     
-    @api.model
-    def _default_anio(self):
-        return time.strftime('%Y')   
+      
     name = fields.Char(string='Descripción', required=True, compute='_compute_name',)
     servicio_id = fields.Many2one(string='Servicio', comodel_name='mz.asignacion.servicio', ondelete='restrict',domain="[('programa_id', '=?', programa_id)]", required=True, tracking=True)  
     personal_id = fields.Many2one(string='Personal', comodel_name='hr.employee', ondelete='restrict',) 
     
     # hay que eliminar estos campos ya que fuerin reemplados por fecha inicio y fecha de fin
-    mes_genera = fields.Selection([('1', 'ENERO'), ('2', 'FEBRERO'), ('3', 'MARZO'), ('4', 'ABRIL'), ('5', 'MAYO'), ('6', 'JUNIO'), ('7', 'JULIO'), ('8', 'AGOSTO'), 
-                                   ('9', 'SEPTIEMBRE'), ('10', 'OCTUBRE'), ('11', 'NOVIEMBRE'), ('12', 'DICIEMBRE')],string='Mes a Generar')
-    anio = fields.Char(string='Año', default=_default_anio)
+    # mes_genera = fields.Selection([('1', 'ENERO'), ('2', 'FEBRERO'), ('3', 'MARZO'), ('4', 'ABRIL'), ('5', 'MAYO'), ('6', 'JUNIO'), ('7', 'JULIO'), ('8', 'AGOSTO'), 
+    #                                ('9', 'SEPTIEMBRE'), ('10', 'OCTUBRE'), ('11', 'NOVIEMBRE'), ('12', 'DICIEMBRE')],string='Mes a Generar')
+    # anio = fields.Char(string='Año', default=_default_anio)
     maximo_beneficiarios = fields.Integer(string='Máximo Beneficiarios por turno', default=1, required=True)
     turno_disponibles_ids = fields.One2many(string='', comodel_name='mz.planificacion.servicio', inverse_name='generar_horario_id',)
     domain_personal_id = fields.Char(string='Domain Personal',compute='_compute_author_domain_field') 
@@ -39,12 +37,41 @@ class GenerarHorarios(models.Model):
     programa_id = fields.Many2one('pf.programas', string='Programa', required=True, tracking=True, default=lambda self: self.env.programa_id) 
     domain_programa_id = fields.Char(string='Domain Programa',compute='_compute_domain_programas')
     active = fields.Boolean(default=True, string='Activo', tracking=True)
-    _sql_constraints = [('name_unique', 'UNIQUE(servicio_id,personal_id,mes_genera, anio)',
-                         "Ya existe AGENDA para esta persona en el MES seleccionado !!"),]
+    # _sql_constraints = [('name_unique', 'UNIQUE(servicio_id,personal_id)',
+    #                      "Ya existe AGENDA para esta persona en el MES seleccionado !!"),]
     
     fecha_inicio = fields.Date(string='Fecha Inicio', required=True)
     fecha_fin = fields.Date(string='Fecha Fin', required=True)
+
+    es_replanificacion = fields.Boolean('Es Replanificación', default=False)
+    planificacion_original_id = fields.Many2one('nombre.de.tu.modelo', 'Planificación Original')
+    estado = fields.Selection([
+        ('borrador', 'Borrador'),
+        ('confirmado', 'Confirmado'),
+        ('replanificado', 'Replanificado'),
+        ('cancelado', 'Cancelado')
+    ], string='Estado', default='borrador')
     
+    # Constrain que valida que no existan planificacion que se crucen entre fechas de la misma persona y servicio 
+    @api.constrains('servicio_id', 'personal_id', 'fecha_inicio', 'fecha_fin')
+    def _check_overlapping_schedules(self):
+        for record in self:
+            # Buscar planificaciones existentes para la misma persona y servicio
+            overlapping_schedules = self.search([
+                ('id', '!=', record.id),  # Excluir el registro actual
+                ('servicio_id', '=', record.servicio_id.id),
+                ('personal_id', '=', record.personal_id.id),
+                # Condiciones para detectar superposición
+                ('fecha_inicio', '<=', record.fecha_fin),
+                ('fecha_fin', '>=', record.fecha_inicio)
+            ])
+            
+            if overlapping_schedules:
+                raise UserError(
+                    f"No se puede crear la planificación. Ya existe una planificación para el servicio {record.servicio_id.name} "
+                    f"con el personal {record.personal_id.name} que se superpone en el rango de fechas."
+                )
+
     @api.depends('servicio_id')
     def _compute_domain_programas(self):
         for record in self:
@@ -66,13 +93,13 @@ class GenerarHorarios(models.Model):
             else:
                 record.domain_personal_id = [('id', 'in', [])]
     
-    @api.depends('servicio_id', 'personal_id', 'mes_genera', 'anio')
+    @api.depends('servicio_id', 'personal_id', 'fecha_inicio', 'fecha_fin')
     def _compute_name(self):
         for record in self:
-            if record.servicio_id and record.personal_id and record.mes_genera and record.anio:
-                mes_dict = dict(self.fields_get(allfields=['mes_genera'])['mes_genera']['selection'])
-                mes_nombre = mes_dict.get(record.mes_genera, '')
-                record.name = record.servicio_id.name + " - " + record.personal_id.name + " - " + mes_nombre + " - " + record.anio
+            if record.servicio_id and record.personal_id and record.fecha_inicio and record.fecha_fin:
+                # mes_dict = dict(self.fields_get(allfields=['mes_genera'])['mes_genera']['selection'])
+                # mes_nombre = mes_dict.get(record.mes_genera, '')
+                record.name = record.servicio_id.name + " - " + record.personal_id.name + " - " + str(record.fecha_inicio) + " a " + str(record.fecha_fin)
             else:
                 record.name = "Generar Horarios"
 
@@ -90,46 +117,48 @@ class GenerarHorarios(models.Model):
             if not (record.servicio_id) or not (record.personal_id):
                 raise UserError("Debe seleccionar UNIDAD MEDICA - ESPECIALIDAD Y MEDICO!!")
 
-    @api.constrains('mes_genera')
-    def _check_mes_genera(self):
+    @api.constrains('fecha_inicio','fecha_fin')
+    def _check_fecha(self):
         for record in self:
-            if not (record.mes_genera):
-                raise UserError("Debe elegir el MES A GENERAR!!")
+            if not record.fecha_inicio or not record.fecha_fin:
+                raise UserError("Debe elegir las FECHAS A GENERAR!!")
             
-    @api.onchange('anio')
-    def _onchange_anio(self):
+    @api.onchange('fecha_inicio')
+    def _onchange_fecha_inicio(self):
         for record in self:
-            #capturame el año actual en una variable
-            anio_actual = time.strftime('%Y')
-            if anio_actual > record.anio:
-                raise UserError("El año a generar no puede ser menor al año actual!!")
+            if record.fecha_inicio:
+                fecha_actual = fields.Date.today()
+                if fecha_actual > record.fecha_inicio:
+                    raise UserError("La fecha de inicio no puede ser menor al la fecha actual!!")
 
     @api.onchange('servicio_id')
     def _onchange_servicio_id(self):
         for record in self:
             record.personal_id = False
-            record.mes_genera = False
+            record.fecha_inicio = False
+            record.fecha_fin = False
             record.turno_disponibles_ids = False
 
     @api.onchange('personal_id')
     def _onchange_personal_id(self):
         for record in self:
-            record.mes_genera = False
+            record.fecha_inicio = False
+            record.fecha_fin = False
             record.turno_disponibles_ids = False
             
-    def obtener_dias_del_mes(self, mes, anio):
-        # Abril, junio, septiembre y noviembre tienen 30
-        if mes in [4, 6, 9, 11]:
-            return 30
-        # Febrero depende de si es o no bisiesto
-        if mes == 2:
-            if self.es_bisiesto(anio):
-                return 29
-            else:
-                return 28
-        else:
-            # En caso contrario, tiene 31 días
-            return 31
+    # def obtener_dias_del_mes(self, mes, anio):
+    #     # Abril, junio, septiembre y noviembre tienen 30
+    #     if mes in [4, 6, 9, 11]:
+    #         return 30
+    #     # Febrero depende de si es o no bisiesto
+    #     if mes == 2:
+    #         if self.es_bisiesto(anio):
+    #             return 29
+    #         else:
+    #             return 28
+    #     else:
+    #         # En caso contrario, tiene 31 días
+            # return 31
 
     # Boton para generar horas
     def action_generar_horas(self):
@@ -146,11 +175,6 @@ class GenerarHorarios(models.Model):
             # Asumiendo que agregamos campos para fecha_inicio y fecha_fin de la semana
             fecha_inicio = record.fecha_inicio
             fecha_fin = record.fecha_fin
-            
-            # Validar que el rango sea de una semana
-            diferencia_dias = (fecha_fin - fecha_inicio).days
-            if diferencia_dias > 7:
-                raise UserError("El rango de fechas debe ser de máximo 7 días")
                 
             maximo_beneficiarios = record.maximo_beneficiarios
             
@@ -190,45 +214,6 @@ class GenerarHorarios(models.Model):
                     
         return True
 
-    # def action_generar_horas(self):
-    #     for record in self:
-    #         record.turno_disponibles_ids = False
-    #         horarios = self.env['mz.horarios.servicio'].search([('servicio_id', '=', record.servicio_id.id), ('personal_id', '=', record.personal_id.id)],limit=1)
-    #         if not horarios:
-    #             raise UserError("No se ha registrado horarios para este empleado en este servicio!!")
-    #         mes = int(record.mes_genera)
-    #         anio = int(record.anio)
-    #         dias = record.obtener_dias_del_mes(mes, anio)
-    #         maximo_beneficiarios = record.maximo_beneficiarios
-    #         for horario in horarios:        
-    #             for dia in range(dias):
-    #                 # fecha_asignar = datetime.strptime(f'{str(anio)}-{str(mes)}-{str(dia + 1)}', '%Y-%m-%d')
-    #                 text = str(anio)+"-"+str(mes)+"-"+str(dia+1)
-    #                 fecha_asignar = datetime.datetime.strptime(
-    #                     text, "%Y-%m-%d")
-    #                 ultimo_dia_asignar = fecha_asignar.weekday()
-    #                 #raise UserError("a {}".format(ultimo_dia_asignar))
-    #                 horas_dia = self.env['mz.detalle.horarios'].search([('asignacion_horario_id', '=', horario.id), ('dias', '=', ultimo_dia_asignar)])                    
-    #                 if horas_dia:
-    #                     for horas in horas_dia:
-                            
-    #                         hora_ini = horas.horainicio
-    #                         hi = horas.horainicio * 60
-    #                         h1, m1 = divmod(hi, 60)
-    #                         horafin = horas.horafin
-    #                         hf = horas.horafin * 60
-    #                         h2, m2 = divmod(hf, 60)
-    #                         duracion = horas.duracionconsulta
-    #                         # ahora si estamos
-    #                         #raise UserError("si estamos {}".format(horas))
-    #                         hora = '%02d:%02d-%02d:%02d' % (h1, m1, h2, m2)
-    #                         #raise UserError("h1 {} m1 {} h2 {} m2 {}".format(h1, m1, h2, m2))
-    #                         while round(hora_ini + duracion, 2) <= round(horafin, 2):
-    #                             record.turno_disponibles_ids = [(0, 0, {'fecha': fecha_asignar, 'horainicio': hora_ini, 'horafin': hora_ini +
-    #                                                                duracion, 'hora': hora, 'maximo_beneficiarios': maximo_beneficiarios})]
-                                
-    #                             hora_ini = hora_ini + duracion 
-    #     return True
 
     # generar la planificacion de las horas de las citas disponibles por especialidad, unidadmedica y medico
     @api.onchange('fecha_inicio', 'fecha_fin')
@@ -238,22 +223,50 @@ class GenerarHorarios(models.Model):
                 if record.fecha_inicio > record.fecha_fin:
                     raise UserError("La fecha de inicio no puede ser mayor a la fecha de fin")
                 self.action_generar_horas()
-                    #raise UserError("a")
             else:
                 record.turno_disponibles_ids = False
 
-    @api.onchange('personal_id')
-    def _onchange_doctor(self):
-        for record in self:
-            record.mes_genera = False
+    def action_replanificar(self):
+        """Crear una nueva planificación basada en la actual"""
+        self.ensure_one()
+        if self.estado != 'confirmado':
+            raise UserError('Solo se pueden replanificar turnos confirmados')
+            
+        # Crear nueva planificación
+        nueva_planificacion = self.copy({
+            'es_replanificacion': True,
+            'planificacion_original_id': self.id,
+            'estado': 'borrador',
+            'fecha_inicio': False,  # Se establecerá en el wizard
+            'fecha_fin': False,     # Se establecerá en el wizard
+            'turno_disponibles_ids': False  # Los turnos se generarán después
+        })
+        
+        # Abrir wizard para configurar las nuevas fechas
+        return {
+            'name': _('Replanificar Turnos'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'mz.wizard.replanificar.turnos',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_planificacion_id': nueva_planificacion.id,
+                'default_planificacion_original_id': self.id
+            }
+        }
+    
+    def action_confirmar(self):
+        self.ensure_one()
+        if self.es_replanificacion and self.planificacion_original_id:
+            self.planificacion_original_id.estado = 'replanificado'
+        self.estado = 'confirmado'
+        
+    def action_cancelar(self):
+        self.ensure_one()
+        self.estado = 'cancelado'
 
-    @api.onchange('anio')
-    def _onchange_anio(self):
-        for record in self:
-            record.mes_genera = False   
-
-    def es_bisiesto(self, anio):
-        return anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0) 
+    # def es_bisiesto(self, anio):
+    #     return anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0) 
 
 
     
@@ -296,3 +309,41 @@ class PlanificacionServicio(models.Model):
                 record.horario = f"{record.dia} (hora inicio: {hora_inicio}, hora fin: {hora_fin})"
             else:
                 record.horario = ''
+
+
+class WizardReplanificarTurnos(models.TransientModel):
+    _name = 'mz.wizard.replanificar.turnos'
+    _description = 'Wizard para Replanificar Turnos'
+    
+    planificacion_id = fields.Many2one('nombre.de.tu.modelo', 'Nueva Planificación')
+    planificacion_original_id = fields.Many2one('nombre.de.tu.modelo', 'Planificación Original')
+    fecha_inicio = fields.Date('Nueva Fecha Inicio', required=True)
+    fecha_fin = fields.Date('Nueva Fecha Fin', required=True)
+    motivo_replanificacion = fields.Text('Motivo de Replanificación', required=True)
+    
+    @api.onchange('fecha_inicio')
+    def _onchange_fecha_inicio(self):
+        if self.fecha_inicio:
+            self.fecha_fin = self.fecha_inicio + timedelta(days=6)
+    
+    def action_replanificar(self):
+        self.ensure_one()       
+        if self.fecha_inicio < fields.Date.today():
+            raise UserError('No se puede replanificar para fechas pasadas')
+        
+        # Actualizar la nueva planificación
+        self.planificacion_id.write({
+            'fecha_inicio': self.fecha_inicio,
+            'fecha_fin': self.fecha_fin
+        })
+        
+        # Generar nuevos turnos
+        self.planificacion_id.action_generar_horas()
+        
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'nombre.de.tu.modelo',
+            'res_id': self.planificacion_id.id,
+            'view_mode': 'form',
+            'target': 'current',
+        }
